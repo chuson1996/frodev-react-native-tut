dd0c5eb9b3d36163e025922e595c113e9d6f7309
Chu Hoang Son <chuson1996@gmail.com>
2017-07-30 17:31:31 +0300

    quotes generator

--- App.js
+++ App.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
+import { Font, LinearGradient } from 'expo';
+
+
+const { width } = Dimensions.get('window');
+
+// Gradient colors are taken from https://webgradients.com/
+const quotes = [{
+  message: 'Stop focusing on dumb shit. Don’t be afraid to break things. Don’t be romantic. Don’t take the time to breathe. Don’t aim for perfect. And whatever you do, keep moving.',
+  author: 'Gary Vaynerchuk',
+  gradientBackground: ['#ff9a9e', '#fad0c4'],
+}, {
+  message: 'Make it simple. Make it memorable. Make it inviting to look at. Make it fun to read.',
+  author: 'Gary Vaynerchuck',
+  gradientBackground: ['#a18cd1', '#fbc2eb'],
+}];
 
 export default class App extends React.Component {
+  state = {
+    quoteIndex: 0,
+    fontLoaded: false,
+  };
+
+  componentDidMount() {
+    Font.loadAsync({
+      'Prata': require('./assets/fonts/Prata-Regular.ttf'),
+      'Average': require('./assets/fonts/Average-Regular.ttf'),
+    }).then(() => {
+      this.setState({
+        fontLoaded: true,
+      });
+    });
+  }
+
+  randomizeQuote = () => {
+    this.setState({
+      quoteIndex: Math.floor(Math.random() * 10) % quotes.length
+    });
+  }
+
   render() {
+    const { quoteIndex, fontLoaded } = this.state;
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <LinearGradient colors={quotes[quoteIndex].gradientBackground} style={styles.container}>
+        <Text style={[styles.message, fontLoaded && { fontFamily: 'Prata' }]}>{quotes[quoteIndex].message}</Text>
+        <Text style={[styles.author, fontLoaded && { fontFamily: 'Average' }]}>{quotes[quoteIndex].author}</Text>
+
+        <View style={styles.button}>
+          <Button title={'Regenerate quote'} onPress={this.randomizeQuote}/>
+        </View>
+      </LinearGradient>
     );
   }
 }
@@ -16,8 +58,24 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20
+  },
+  message: {
+    backgroundColor: 'transparent',
+    fontSize: 26,
+    marginBottom: 20,
+  },
+  author: {
+    backgroundColor: 'transparent',
+    fontSize: 18,
+    width: width - 40,
+    textAlign: 'right',
+  },
+  button: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    bottom: 30,
   },
 });
